e5ac22aaed5074b2f57d5c238302aca659f3614b
vchertkov <vchertkov@ayapy.pw>
2016-02-28 17:10:05 +0300

    1

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo rm -rf /etc/nginx/nginx.conf
-sudo ﻿ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ﻿ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
