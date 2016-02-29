c3f19284a28bdc84df4f88625da581c4652e0763
vchertkov <vchertkov@ayapy.pw>
2016-02-29 21:40:46 +0300

    1

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ﻿ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/gunicorn.conf
+sudo /etc/init.d/gunicorn restart
